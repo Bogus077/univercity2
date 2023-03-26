d29978ebddba31e2fe9df2bba391131788e5ff75
Vladislav Sharkov <bogus077@gmail.com>
2023-03-27 01:34:52 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import './assets/fonts/style.css';
+import './assets/global.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<>324</>} />
+
+        {/* <Route
+        path={frontendRoutes.index}
+        element={<DashboardPage />}
+      /> */}
+
+        {/* Участники */}
+        {/* <Route
+        path={frontendRoutes.kids.list}
+        element={<KidListPage />}
+      />
+      <Route
+        path={`${frontendRoutes.kids.edit}/:id`}
+        element={<EditKidPage />}
+      />  */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
